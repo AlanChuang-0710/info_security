a74f5893944289fd099ae41ee5a26b766ea1adb0
jessy shen <jessy.shen@flowring.com>
2022-06-02 10:57:13 +0800

    優化 listItem

--- src/components/lists/ListItem.vue
+++ src/components/lists/ListItem.vue
@@ -102,7 +102,10 @@ export default {
 </script>
 <template>
   <div>
-    <q-card-section class="text-dark row q-col-gutter-md" style="background-color: #363636;">
+    <q-card-section
+      class="text-dark row q-col-gutter-md"
+      style="background-color: #363636"
+    >
       <div class="col-md-4 col-xs-12" v-for="(item, idx) in data" :key="idx">
         <div class="flex justify-between items-center q-px-md q-mb-md">
           <div class="text-white">{{ item.type }}</div>
@@ -117,42 +120,48 @@ export default {
           ></q-btn>
         </div>
         <q-list>
-          <q-item class="bg-white q-mb-sm" style="border-radius: 5px" v-for="(ele, id) in item.detail" :key="id">
+          <q-item
+            class="bg-white q-mt-md q-mb-sm"
+            style="border-radius: 5px"
+            v-for="(ele, id) in item.detail"
+            :key="id"
+          >
             <q-item-section>{{ ele.title }}</q-item-section>
             <q-item-section avatar>
-              <div :style="`border-bottom: 2px solid ${ele.color}`">{{ ele.num }}</div>
+              <div :style="`border-bottom: 2px solid ${ele.color}`">
+                {{ ele.num }}
+              </div>
             </q-item-section>
           </q-item>
         </q-list>
+        <!-- expansion -->
+        <!-- <q-expansion-item
+            dark
+            dense-toggle
+            default-opened
+            expand-icon-class="#fff"
+            expand-separator
+            label="Asset Reputation"
+            :header-style="{ borderBottom: '1px solid #fff' }"
+        >
+          <q-card>
+            <q-card-section>
+              <div class="text-dark">詳細資料</div>
+            </q-card-section>
+          </q-card>
+        </q-expansion-item> -->
+
+        <!-- dropdown -->
+        <!-- <q-btn-dropdown color="primary" label="Dropdown Button">
+          <q-list>
+            <q-item clickable v-close-popup>
+              <q-item-section>
+                <q-item-label>Photos</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown> -->
       </div>
-      <!-- <div class="col-md-4 col-xs-12">
-        <div class="flex justify-between items-center q-px-sm">
-          <div>Network and IT</div>
-          <q-btn
-            class="cursor-inherit"
-            no-caps
-            :ripple="false"
-            round
-            label="95"
-            size="10px"
-            color="warning"
-          ></q-btn>
-        </div>
-        <q-list>
-          <q-item class="bg-negative q-mb-sm" style="border-radius: 5px">
-            <q-item-section>Asset Reputation</q-item-section>
-            <q-item-section avatar>
-              <div style="border-bottom: 2px solid green">100</div>
-            </q-item-section>
-          </q-item>
-          <q-item class="bg-negative q-mb-sm" style="border-radius: 5px">
-            <q-item-section>Asset Reputation</q-item-section>
-            <q-item-section avatar>
-              <div style="border-bottom: 2px solid green">100</div>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </div> -->
     </q-card-section>
   </div>
 </template>
