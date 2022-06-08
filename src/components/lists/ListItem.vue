0753ed536043bb658698329a22a0345c36471d18
jessy shen <jessy.shen@flowring.com>
2022-06-08 17:24:20 +0800

    優化列表警示訊息

--- src/components/lists/ListItem.vue
+++ src/components/lists/ListItem.vue
@@ -3,6 +3,7 @@ export default {
   name: "ListItem",
   data() {
     return {
+      modal: false,
       data: [
         {
           type: "Network and IT",
@@ -12,26 +13,30 @@ export default {
             {
               title: "Certification & Risk Assessment",
               num: 100,
-              color: "green",
+              color: "red",
               content: [
                 {
                   context: "Awareness training and promotion",
+                  color: "green",
                 },
                 {
                   context: "Security preventive rehearsal",
+                  color: "red",
                 },
               ],
             },
             {
               title: "Inventory Management & Physical Security",
               num: 45,
-              color: "red",
+              color: "green",
               content: [
                 {
                   context: "Inventory Management",
+                  color: "",
                 },
                 {
                   context: "Physical Security",
+                  color: "",
                 },
               ],
             },
@@ -42,9 +47,11 @@ export default {
               content: [
                 {
                   context: "SOC operation",
+                  color: "",
                 },
                 {
                   context: "Incident response management",
+                  color: "",
                 },
               ],
             },
@@ -55,9 +62,11 @@ export default {
               content: [
                 {
                   context: "AP Security guidelines",
+                  color: "",
                 },
                 {
                   context: "Security Code Scan",
+                  color: "",
                 },
               ],
             },
@@ -68,18 +77,23 @@ export default {
               content: [
                 {
                   context: "Security team arrangement / policy readiness",
+                  color: "",
                 },
                 {
                   context: "Risk assessment management",
+                  color: "",
                 },
                 {
                   context: "Employment background checks / NDA",
+                  color: "",
                 },
                 {
                   context: "Training and awareness Promotion",
+                  color: "",
                 },
                 {
                   context: "Phising mail testing",
+                  color: "",
                 },
               ],
             },
@@ -90,18 +104,23 @@ export default {
               content: [
                 {
                   context: "BYOD security control",
+                  color: "",
                 },
                 {
                   context: "IT asset management / Security Hardening",
+                  color: "",
                 },
                 {
                   context: "Patch management / Anti-virus protection",
+                  color: "",
                 },
                 {
                   context: "Data protection / Media protection",
+                  color: "",
                 },
                 {
                   context: "Access control / Remote access management",
+                  color: "",
                 },
               ],
             },
@@ -112,15 +131,19 @@ export default {
               content: [
                 {
                   context: "Authentication and authorization",
+                  color: "",
                 },
                 {
                   context: "Account / password policy",
+                  color: "",
                 },
                 {
                   context: "Privilege Account management",
+                  color: "",
                 },
                 {
                   context: "Non-organizational user management",
+                  color: "",
                 },
               ],
             },
@@ -131,18 +154,23 @@ export default {
               content: [
                 {
                   context: "Boundary protection (DMZ)",
+                  color: "",
                 },
                 {
                   context: "Network compartment (Intranet)",
+                  color: "",
                 },
                 {
                   context: "Internet access protection",
+                  color: "",
                 },
                 {
                   context: "Email protection",
+                  color: "",
                 },
                 {
                   context: "Cloud security",
+                  color: "",
                 },
               ],
             },
@@ -246,6 +274,8 @@ export default {
             <q-item-section>
               <div class="ellipsis" style="width: 90%">{{ ele.title }}</div>
             </q-item-section>
+
+            <div class="circle" :style="`background-color: ${ele.color}`"></div>
             <!-- <q-item-section
               side
               :style="`border-bottom: 2px solid ${ele.color}; padding-left: 0!important;`"
@@ -257,7 +287,15 @@ export default {
             <q-card-section>
               <ul class="text-dark">
                 <li v-for="(sector, id) in ele.content" :key="id">
-                  {{ sector.context }}
+                  <div class="flex items-center">
+                    <div @click="modal = true" class="cursor-pointer">
+                      {{ sector.context }}
+                    </div>
+                    <div
+                      class="circle-2 q-ml-md"
+                      :style="`background-color: ${sector.color}`"
+                    ></div>
+                  </div>
                 </li>
               </ul>
             </q-card-section>
@@ -265,10 +303,32 @@ export default {
         </q-expansion-item>
       </div>
     </q-card-section>
+    <q-dialog v-model="modal">
+      <q-card bordered style="width: 350px; max-width: 350px">
+        <q-card-section class="flex column justify-center items-center">
+          <q-btn round color="negative" icon="error_outline" class="q-mb-sm" />
+          <div class="text-h5 q-mt-sm q-mb-xs">Problem Issue</div>
+          <p>Information security is a problem</p>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 <style lang="scss" scoped>
 ul {
   padding-inline-start: 20px;
+}
+.circle {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  transform: translateY(100%);
+}
+.circle-2 {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
 }
 </style>
